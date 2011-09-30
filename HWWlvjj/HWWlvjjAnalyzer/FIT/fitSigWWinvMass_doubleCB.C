a7c1d68b440223fa017897a1c8e3cf090317c0ef
pandolf <>
2011-09-30 17:40:15 +0000

    *** empty log message ***

--- HWWlvjj/HWWlvjjAnalyzer/FIT/fitSigWWinvMass_doubleCB.C
+++ HWWlvjj/HWWlvjjAnalyzer/FIT/fitSigWWinvMass_doubleCB.C
@@ -36,13 +36,13 @@ vector<double> fitSigWWinvMass_doubleCB(int iSig=2, int fixVar=0) {
   //setTDRStyle();
   // --------------------- initial values -----------------
 
-  double meanIV[8]={200.,300.,350.,400.,450.,500.,550.,600.};
-  double sigmaIV[8]={1.43,8.445,15.39,29.42,47.01,67.52,93.15,123.0};
-  double effWidth[8]={10.1,13.12,18.36,31.05,48.1,68.26,93.7,123.41};
+  double meanIV[7]={200.,300.,350.,400.,450.,500.,550.};
+  double sigmaIV[7]={1.43,8.445,15.39,29.42,47.01,67.52,93.15};
+  double effWidth[7]={10.1,13.12,18.36,31.05,48.1,68.26,93.7};
   double fitRangeLow= meanIV[iSig]-10*effWidth[iSig]<150?150:meanIV[iSig]-10*effWidth[iSig];
   double fitRangeHigh=meanIV[iSig]+10*effWidth[iSig]>800?800:meanIV[iSig]+10*effWidth[iSig];
   string plotName;
-  string massName[8]={"200","300","350","400","450","500","550","600"};
+  string massName[7]={"200","300","350","400","450","500","550"};
   string cutString = "mJJ>60. && mJJ<100.";
 
   float mass = meanIV[iSig];
@@ -56,32 +56,34 @@ vector<double> fitSigWWinvMass_doubleCB(int iSig=2, int fixVar=0) {
   cout << "declared tree vars" << endl;
   // ====================== defining signal PDF =========================
 
-  vector<double> param;
-    param.push_back(70.6146-.697703*meanIV[iSig]+0.00212559*meanIV[iSig]*meanIV[iSig]-0.00000180624*meanIV[iSig]*meanIV[iSig]*meanIV[iSig]);  
-    param.push_back(-5.967+0.05885*meanIV[iSig]-0.00006977*meanIV[iSig]*meanIV[iSig]);				    
-    param.push_back(1.0);									    
-    param.push_back(3.38183-0.00421732*meanIV[iSig]);							    
-    param.push_back(1.0);									    
-    param.push_back(-1.37066+0.0190719*meanIV[iSig]-0.0000250673*meanIV[iSig]*meanIV[iSig]);                             
+  //float CB_mean_0 = 70.6146-.697703*mass+0.00212559*mass*mass-0.00000180624*mass*mass*mass;
+  float CB_mean_0 = -8.52531+5.88265e-03*mass+1.10855e-04*mass*mass;
+  float CB_sigma_0 = -1.63923+8.50605e-02*mass-1.32823e-04*mass*mass; //fp
+  //float CB_alpha1_0 = 1.0;
+  float CB_alpha1_0 = 0.85;
+  float CB_n1_0 = 3.38183-0.00421732*mass;
+  float CB_alpha2_0 = 2.82669-1.21212e-02*mass+1.84626e-05*mass*mass; //fp
+  float CB_n2_0 = -1.37066+0.0190719*mass-0.0000250673*mass*mass;
 
-  for(int i=0; i<param.size(); i++){
-    cout << "param[" << i << "]: " << param.at(i) << endl;
-  }
+
+//for(int i=0; i<param.size(); i++){
+//  cout << "param[" << i << "]: " << param.at(i) << endl;
+//}
 
   // -------------------- fermi ------------------------
   
-  RooRealVar cutOff("cutOff","cutOff",190-32.5+65*meanIV[iSig]/400,0,1000); //param[6],0,1000);
+  RooRealVar cutOff("cutOff","cutOff",190.-32.5+65.*meanIV[iSig]/400.,0.,1000.); //param[6],0,1000);
   cutOff.setConstant(kTRUE);
-  RooRealVar g("g","g",5-12.5+25*meanIV[iSig]/400,0,100); //param[7],0,100);
+  RooRealVar g("g","g",5.-12.5+25.*meanIV[iSig]/400.,0.,100.); //param[7],0,100);
   g.setConstant(kTRUE);
 
   RooFermi fermi("fermi","fermi",mWW,cutOff,g);
 
   // ------------------- fermi for high mass cutoff --------------
 
-  RooRealVar cutOff2("cutOff2","cutOff2",700,0,1000); //param[6],0,1000);
+  RooRealVar cutOff2("cutOff2","cutOff2",700.,0.,1000.); //param[6],0,1000);
   cutOff2.setConstant(kTRUE);
-  RooRealVar g2("g2","g2",-70,-100,0); //param[7],0,100);
+  RooRealVar g2("g2","g2",-70.,-100.,0.); //param[7],0,100);
   g2.setConstant(kTRUE);
 
   RooFermi fermi2("fermi2","fermi2",mWW,cutOff2,g2);
@@ -89,7 +91,7 @@ vector<double> fitSigWWinvMass_doubleCB(int iSig=2, int fixVar=0) {
   // ------------------- Relativistic BW --------------------------------
   //
  
-  RooRealVar BW_mean("BW_mean", "mean",meanIV[iSig],0,1000);
+  RooRealVar BW_mean("BW_mean", "mean",meanIV[iSig],0.,1000.);
   BW_mean.setConstant(kTRUE);
   RooRealVar BW_sigma("BW_sigma", "sigma",sigmaIV[iSig],0,200);
   BW_sigma.setConstant(kTRUE);
@@ -103,17 +105,17 @@ vector<double> fitSigWWinvMass_doubleCB(int iSig=2, int fixVar=0) {
   RooRelBW BW("BW","Relativistic B-W",mWW,BW_mean,BW_sigma,BW_n);
 
   // ------------------- Crystal Ball -------------------------------
-  RooRealVar CB_mean("CB_mean","param 1 of CB",param[0],0.,100.);
+  RooRealVar CB_mean("CB_mean","param 1 of CB",CB_mean_0,0.,100.);
 //CB_mean.setConstant(kTRUE);
-  RooRealVar CB_sigma("CB_sigma","param 2 of CB",param[1],0.,100.);
+  RooRealVar CB_sigma("CB_sigma","param 2 of CB",CB_sigma_0,0.,100.);
 //CB_sigma.setConstant(kTRUE);
-  RooRealVar CB_alpha1("CB_alpha1","param 3 of CB",param[2],0.,100.);
+  RooRealVar CB_alpha1("CB_alpha1","param 3 of CB",CB_alpha1_0,0.,100.);
 //CB_alpha1.setConstant(kTRUE);
-  RooRealVar CB_n1("CB_n1","param 4 of CB",param[3],0.,100.);
+  RooRealVar CB_n1("CB_n1","param 4 of CB",CB_n1_0,0.,100.);
 //CB_n1.setConstant(kTRUE);
-  RooRealVar CB_alpha2("CB_alpha2","param 3 of CB",param[4],0.,100.);
+  RooRealVar CB_alpha2("CB_alpha2","param 3 of CB",CB_alpha2_0,0.,100.);
 //CB_alpha2.setConstant(kTRUE);
-  RooRealVar CB_n2("CB_n2","param 4 of CB",param[5],0.,100.);
+  RooRealVar CB_n2("CB_n2","param 4 of CB",CB_n2_0,0.,100.);
 //CB_n2.setConstant(kTRUE);
 
 //if(fixVar==1) CB_mean.setConstant(kFALSE);
@@ -134,8 +136,9 @@ vector<double> fitSigWWinvMass_doubleCB(int iSig=2, int fixVar=0) {
   RooFFTConvPdf sig("sig","Rel-BW (X) CB",mWW,BW,CB);
   sig.setBufferFraction(1.0);
   
-  //RooProdPdf signal("signal","signal",RooArgSet(sig,fermi,fermi2));
-  RooProdPdf signal("signal","signal",RooArgSet(sig));
+  RooProdPdf signal("signal","signal",RooArgSet(sig,fermi,fermi2));
+  //RooProdPdf signal("signal","signal",RooArgSet(sig));
+  //RooProdPdf signal("signal","signal",RooArgSet(CB));
 
   cout << "PDFs defined" << endl;
 
@@ -168,16 +171,19 @@ vector<double> fitSigWWinvMass_doubleCB(int iSig=2, int fixVar=0) {
   string outFileName="HWWlvjj_signal_"+massName[iSig]+"GeV_parameters.txt";
 
   ofstream output(outFileName.c_str());
-  output << CB_mean.GetName() << ": " << CB_mean.getVal() << " +/- " << CB_mean.getError() << endl;
-  output << CB_sigma.GetName() << ": " << CB_sigma.getVal() << " +/- " << CB_sigma.getError() << endl;
-  output << CB_alpha1.GetName() << ": " << CB_alpha1.getVal() << " +/- " << CB_alpha1.getError() << endl;
-  output << CB_n1.GetName() << ": " << CB_n1.getVal() << " +/- " << CB_n1.getError() << endl;
-  output << CB_alpha2.GetName() << ": " << CB_alpha2.getVal() << " +/- " << CB_alpha2.getError() << endl;
-  output << CB_n2.GetName() << ": " << CB_n2.getVal() << " +/- " << CB_n2.getError() << endl;
-  output << cutOff.GetName() << ": " << cutOff.getVal() << " +/- " << cutOff.getError() << endl;
-  output << g.GetName() << ": " << g.getVal() << " +/- " << g.getError() << endl;
-  output << cutOff2.GetName() << ": " << cutOff2.getVal() << " +/- " << cutOff2.getError() << endl;
-  output << g2.GetName() << ": " << g2.getVal() << " +/- " << g2.getError() << endl;
+  output << BW_mean.GetName() << " " << BW_mean.getVal() << " " << BW_mean.getError() << endl;
+  output << BW_sigma.GetName() << " " << BW_sigma.getVal() << " " << BW_sigma.getError() << endl;
+  output << BW_n.GetName() << " " << BW_n.getVal() << " " << BW_n.getError() << endl;
+  output << CB_mean.GetName() << " " << CB_mean.getVal() << " " << CB_mean.getError() << endl;
+  output << CB_sigma.GetName() << " " << CB_sigma.getVal() << " " << CB_sigma.getError() << endl;
+  output << CB_alpha1.GetName() << " " << CB_alpha1.getVal() << " " << CB_alpha1.getError() << endl;
+  output << CB_n1.GetName() << " " << CB_n1.getVal() << " " << CB_n1.getError() << endl;
+  output << CB_alpha2.GetName() << " " << CB_alpha2.getVal() << " " << CB_alpha2.getError() << endl;
+  output << CB_n2.GetName() << " " << CB_n2.getVal() << " " << CB_n2.getError() << endl;
+  output << cutOff.GetName() << " " << cutOff.getVal() << " " << cutOff.getError() << endl;
+  output << g.GetName() << " " << g.getVal() << " " << g.getError() << endl;
+  output << cutOff2.GetName() << " " << cutOff2.getVal() << " " << cutOff2.getError() << endl;
+  output << g2.GetName() << " " << g2.getVal() << " " << g2.getError() << endl;
   
   output.close();
 
