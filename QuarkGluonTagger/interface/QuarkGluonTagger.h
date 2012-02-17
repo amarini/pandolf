a34e820d8f9dad7574cc7d531b4d077f7087d395
pandolf <>
2012-02-17 13:40:43 +0000

    first commit

---
+++ QuarkGluonTagger/interface/QuarkGluonTagger.h
@@ -0,0 +1,34 @@
+// system include files
+#include <string>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/FileInPath.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "UserCode/pandolf/QGLikelihood/QGLikelihoodCalculator.h"
+
+
+
+
+class QuarkGluonTagger : public edm::EDProducer {
+   public:
+      explicit QuarkGluonTagger(const edm::ParameterSet&);
+      ~QuarkGluonTagger();
+
+   private:
+      virtual void beginJob() ;
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      // ----------member data --------------------------
+      edm::InputTag src_,srcRho_;
+      std::string result_,jecService_,fileName_;
+      QGLikelihoodCalculator *qglikeli_;
+      const JetCorrector *JEC_;           
+};
+
